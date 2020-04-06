1151ef7428ade8761e2318faba1fd544f8e743ca
nmsbnmsb1 <nmsbnmsb1@126.com>
2020-04-06 22:42:09 +0800

    fix configPath

--- examples/tw-config2.js
+++ examples/tw-config2.js
@@ -1,7 +1,7 @@
 const path = require('path');
 //import utils class
 const utils = require('./../lib');
-const config = utils.pre('light', false, `examples/tw-config.js`);
+const config = utils.pre('light', false, `./examples/tw-config.js`);
 module.exports = config;
 
 utils.delete(utils.keys.screens);
