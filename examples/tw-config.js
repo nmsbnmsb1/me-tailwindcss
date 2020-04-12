bd06b6f39aa4c080c0d5b08abfe59d478a78ed0c
nmsbnmsb1 <nmsbnmsb1@126.com>
2020-04-12 23:29:55 +0800

    fix w-screen/h-screen

--- examples/tw-config.js
+++ examples/tw-config.js
@@ -1,6 +1,7 @@
 //import utils class
+const path = require('path');
 const utils = require('./../lib');
-const config = utils.pre('default:dark');
+const config = utils.pre('default:dark', false, undefined);
 module.exports = config;
 
 //
@@ -12,81 +13,31 @@ utils.addPlugins(
     ['backgrounds', 'bg', 'background'],
   ])
 );
-//now it's time to add some config
-/* Screen -----------------------------------------------------------------------    */
-utils.delete(utils.keys.screens);
-utils.group(utils.keys.screens, {
-  sm: { max: '991px' },
-  lg: '992px',
-});
 
-/* Sizes -----------------------------------------------------------------------    */
-utils.setAllSpacing({
-  '-2': '-2px',
-  '-4': '-4px',
-  '-54': '-14px',
-  '2': '2px',
-  '3': '3px',
-  '10%': '10%',
-});
-
-/* TextSize -----------------------------------------------------------------------    */
-utils.group(utils.keys.fontSize, {
-  xss: '10px',
-  base: '14px',
-  num: '6rem',
-});
-
-/* Box-Shadow -----------------------------------------------------------------------    */
-utils.group(utils.keys.boxShadow, {
-  base: {
-    var: '',
-    val: {
-      dark: '0 0 35px 0 rgba(154, 161, 171, 0.15)',
-      light: '0 0 35px 0 rgba(154, 161, 171, 0.15)',
-    },
-  },
-});
-
-/* Text-Colors -----------------------------------------------------------------------    */
+//colors
+utils.setDefinedTypeColors(
+  { var: '', val: { dark: [60, 40, 20, 10, '#2c7be5', 20, 50, 70, 90] } },
+  { var: '', val: { dark: [60, 40, 20, 10, '#9d7bd8', 20, 50, 70, 90] } },
+  { var: '', val: { dark: [0, 0, 0, 10, '#4caf50', 20, 50, 80, 90] } },
+  { var: '', val: { dark: [0, 0, 0, 10, '#e51c23', 20, 50, 80, 90] } },
+  { var: '', val: { dark: [0, 0, 0, 10, '#ff9800', 20, 50, 80, 90] } },
+  { var: '', val: { dark: [0, 0, 0, 10, '#47bac1', 20, 50, 80, 90] } }
+);
 utils.setDefinedTextColors(
-  {
-    var: '',
-    val: {
-      dark: [60, 40, 20, 10, '#737b81', 20, 50, 70, 90],
-      light: [60, 40, 20, 10, '#737b81', 20, 50, 70, 90],
-    },
-  },
+  { var: '', val: { dark: [60, 40, 20, 10, '#737b81', 20, 50, 70, 90], light: [60, 40, 20, 10, '#737b81', 20, 50, 70, 90] } },
   '#ffffff',
   '#ffffff',
-  {
-    var: '',
-    val: {
-      dark: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90],
-      light: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90],
-    },
-  },
+  { var: '', val: { dark: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90], light: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90] } },
   '#ffffff'
 );
 utils.setDefinedBorderColors(
-  {
-    var: '',
-    val: {
-      dark: [60, 40, 20, 10, '#e6e6e6', 20, 50, 70, 90],
-      light: [60, 40, 20, 10, '#e6e6e6', 20, 50, 70, 90],
-    },
-  },
+  { var: '', val: { dark: [60, 40, 20, 10, '#e6e6e6', 20, 50, 70, 90], light: [60, 40, 20, 10, '#e6e6e6', 20, 50, 70, 90] } },
   '#ffffff',
   '#ffffff'
 );
 utils.setDefinedBgColors(
-  {
-    var: '',
-    val: {
-      dark: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90],
-      light: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90],
-    },
-  },
+  { var: '', val: { dark: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90], light: [60, 40, 20, 10, '#98a6ad', 20, 50, 70, 90] } },
+  '#ffffff',
   '#ffffff',
   '#ffffff',
   {
