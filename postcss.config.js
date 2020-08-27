8ccf446d879a6cb95a7cadf135199cabc7713384
nmsbnmsb1 <nmsbnmsb1@126.com>
2020-08-27 21:19:19 +0800

    update to 1.7.5

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,20 @@ module.exports = {
     ...require('./lib/postcss-config')({
       //路径从项目根目录开始
       tw: './styles/tailwind.config.js',
-      //路径从项目根目录开始
-      purge: {
-        //add new path
-        content: ['./examples/**/*.vue', './examples/**/*.ts', './examples/**/*.js'],
-        //add new extractors
+      'postcss-preset-env': {
+        stage: 4,
+        preserve: false,
+        features: {
+          //'custom-properties': { preserve: false },
+        },
       },
+      //路径从项目根目录开始
+      purge:false,
+      // purge: {
+      //   //add new path
+      //   content: ['./examples/**/*.vue', './examples/**/*.ts', './examples/**/*.js'],
+      //   //add new extractors
+      // },
       mini: false,
     }),
   ],
